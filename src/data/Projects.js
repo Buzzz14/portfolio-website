5920c56f73dbfe7a5a2bea1caa7c2a18a6bec898
Buzzz14 <sushanbajracharya14@gmail.com>
2024-11-03 08:12:55 +0545

    Minor fixes.

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -6,10 +6,19 @@ export const projects = [
     {
         title: "The Fitness Club",
         description: "It is a website for a gym created using react and tailwind.",
-        src: projectOne,
-        techStack: ["Tailwind", "React", "HTML", "CSS"],
+        src: projectThree,
+        techStack: ["HTML", "CSS"],
         appLink: "https://the-fitness-club-kioo.onrender.com/",
         gitLink: "https://github.com/Buzzz14/The-Fitness-Club/",
+    },
+    {
+        title: "Buzz Cafe",
+        description:
+            "It is a responsive website for a cafe created using plain html and css.",
+        src: projectOne,
+        techStack: ["HTML", "CSS"],
+        appLink: "https://buzz-cafe.onrender.com",
+        gitLink: "https://github.com/Buzzz14/Buzz-Cafe/",
     },
     {
         title: "Dictionary App",
@@ -19,15 +28,6 @@ export const projects = [
         techStack: ["Tailwind", "React", "HTML", "CSS"],
         appLink: "https://dictionary-app-u2q2.onrender.com/",
         gitLink: "https://github.com/Buzzz14/Dictionary-App/",
-    },
-    {
-        title: "Buzz Cafe",
-        description:
-            "It is a responsive website for a cafe created using plain html and css.",
-        src: projectThree,
-        techStack: ["HTML", "CSS"],
-        appLink: "https://buzz-cafe.onrender.com",
-        gitLink: "https://github.com/Buzzz14/Buzz-Cafe/",
     },
 
 ];
